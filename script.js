eaa7e4214004bebabd576a84f96e3ae35e995e77
KaykyPeres <kaykyplombon@gmail.com>
2025-11-23 23:30:26 -0300

    Fix: rolar calendario com o mouse para testes

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let selectedTime = "";
 
 window.onload = function() {
   updateDashboard();
+  enableDragScroll();
 };
 
 function goToScreen(screenId) {
@@ -160,4 +161,38 @@ function showToast(message, type = 'default') {
     toast.style.animation = "toastDown 0.3s forwards";
     setTimeout(() => toast.remove(), 300);
   }, 3000);
+}
+
+function enableDragScroll() {
+  const slider = document.querySelector('.calendar-carousel');
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  if(!slider) return; 
+
+  slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
+
+  slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+  });
+
+  slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+  });
+
+  slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 2; 
+    slider.scrollLeft = scrollLeft - walk;
+  });
 }
